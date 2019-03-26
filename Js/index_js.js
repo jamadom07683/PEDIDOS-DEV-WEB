127dac18560ad8e511d6b193b9e78160c94a1d86
SALA DE CÓMPUTO <SALA@EAN3377.universidad.ean>
2019-03-26 07:53:18 -0500

    Arreglos

--- Js/index_js.js
+++ Js/index_js.js
@@ -1,43 +1,35 @@
-alert("Hola Mundo");
-$(document).ready(function(){
- $('.header').height($(window).height());
+//alert("Hola Mundo");
+console.log("qui azco");
+$(document).ready(function () {
+    $('.header').height($(window).height());
 
 })
 
-function validarFormulario(){
-    jQuery.validator.messages.required = 'Este campo es obligatorio';
-    jQuery.validator.messages.number = 'Este campo debe ser numerico';
-    jQuery.validator.messages.email= 'La direccion de correo es incorrecta';
-    $("#contactForm").validate();
-}
-
-function validar(){
-    var miCampo=document.getElementById("#name1").value;
-    var miTelefono.getElementById("#numer").value;
-    var miCorreo=document.getElementById("#correo").value;
-    var miMensaje=document.getElementById("#mensaje2").value;
+function validar() {
+    var miCampo = document.getElementById("#name1").value;
+    var miTelefono = document.getElementById("#numer").value;
+    var miCorreo = document.getElementById("#correo").value;
+    var miMensaje = document.getElementById("#mensaje2").value;
     valueForm.object.value;
-    var patronCorreo=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
-    if(miCampo.length==0 || /^\s+$/.test(miCampo)){
+    var patronCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
+    if (miCampo.length === 0 || /^\s+$/.test(miCampo)) {
         alert('El campo de texto esta vacio!');
         return false;
     }
     return true;
-    if(valueForm.search(patronCorreo)==0){
+    if (valueForm.search(patronCorreo) == 0) {
         return true
-    }
-    else{
+    } else {
         alert('El correo electronico no es valido')
     }
-    if(miTelefono.length==0 || /^\s+$/.test(miTelefono)){
+    if (miTelefono.length == 0 || /^\s+$/.test(miTelefono)) {
         alert('El campo de texto esta vacio!');
         return false;
     }
     return true
-    if(miMensaje.length==0 || /^\s+$/.test(miMensaje)){
+    if (miMensaje.length == 0 || /^\s+$/.test(miMensaje)) {
         alert('El campo de texto esta vacio!');
         return false;
     }
 
 }
-
