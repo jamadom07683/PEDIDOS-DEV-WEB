7110fa84f68b2519359e816961b77dced15c83b5
jamadom07683 <jamadom07683@universidadean.edu.co>
2019-03-27 18:08:59 -0500

    Validacion espacio nombre

--- Js/index_js.js
+++ Js/index_js.js
@@ -11,10 +11,11 @@ function validar() {
     var miTelefono = document.getElementById("numer").value;
     var miCorreo = document.getElementById("correo").value;
     var miMensaje = document.getElementById("mensaje2").value;
+    var patronTexto=/^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
     var patronCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
     if (miCampo.length === 0 || /^\s+$/.test(miCampo)) {
         alert('El campo de nombre esta vacio!');
-    } else if(miCampo.length > 0 && miCampo.match(/^[a-z]+$/i)=== null){
+    } else if(miCampo.length > 0 && miCampo.match(patronTexto)=== null){
         alert('El nombre debe tener solo letras');
     } else if (miCorreo.length === 0 || /^\s+$/.test(miCorreo)) {
         alert('El campo de correo esta vacio!');
@@ -29,15 +30,16 @@ function validar() {
     }else{
         $(document).ready(function () {
             $("#validateButton").click(function () {
-                $(this).hide();
+                $("#Contacto").load("Enlace.html");
             });
         });
 
         $(document).ready(function () {
             $("#validateButton").click(function () {
-                $("#Contacto").load("Enlace.html");
+                $(this).hide();
             });
         });
+
     }
 }
 
@@ -46,15 +48,6 @@ var buttonValidation = document.getElementById('validateButton');
 buttonValidation.addEventListener('click', validar);
 
 
-/*
-$(document).ready(function () {
-    $("#validateButton").click(function () {
-        $(this).hide();
-    });
-});
 
-$(document).ready(function () {
-    $("#validateButton").click(function () {
-        $("#Contacto").load("Enlace.html");
-    });
-});*/
+
+
