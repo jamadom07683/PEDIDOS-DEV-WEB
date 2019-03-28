67e6ac73fa8823794d39e5c778da463d1baad580
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-03-27 22:41:23 -0500

    Modificación de la función de validación

--- Js/index_js.js
+++ Js/index_js.js
@@ -1,42 +1,38 @@
-$(document).ready(function(){
- $('.header').height($(window).height());
+$(document).ready(function () {
+    $('.header').height($(window).height());
 
 })
 
-function validar(){
-   var miCampo = document.getElementById("name1").value;
+
+/**
+ *
+ * Validación de los campos registrados por el usuario en los formularios.
+ * Al final de la validación muestra un mensaje que expresa que todo lo ingresado corresponde a la informacion que se pidio.
+ *
+ */
+function validar() {
+    var miCampo = document.getElementById("name1").value;
     var miTelefono = document.getElementById("numer").value;
     var miCorreo = document.getElementById("correo").value;
     var miMensaje = document.getElementById("mensaje2").value;
-    var patronTexto=/^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
+    var patronTexto = /^[A-Za-z\_\-\.\s\xF1\xD1]+$/;
     var patronCorreo = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
     if (miCampo.length === 0 || /^\s+$/.test(miCampo)) {
         alert('El campo de nombre esta vacio!');
-    } else if(miCampo.length > 0 && miCampo.match(patronTexto)=== null){
+    } else if (miCampo.length > 0 && miCampo.match(patronTexto) === null) {
         alert('El nombre debe tener solo letras');
     } else if (miCorreo.length === 0 || /^\s+$/.test(miCorreo)) {
         alert('El campo de correo esta vacio!');
-    } else if (miCorreo.length > 0 && miCorreo.match(patronCorreo) === null){
+    } else if (miCorreo.length > 0 && miCorreo.match(patronCorreo) === null) {
         alert('El campo del correo debe ser un correo valido!')
     } else if (miTelefono.length == 0 || /^\s+$/.test(miTelefono)) {
         alert('El campo de telefono esta vacio!');
-    } else if(miTelefono.length>0 && miTelefono.length !== 7 && miTelefono.length !==10){
+    } else if (miTelefono.length > 0 && miTelefono.length !== 7 && miTelefono.length !== 10) {
         alert('El campo de telefono debe contener 7 o 10 digitos!')
-    }else if (miMensaje.length == 0 || /^\s+$/.test(miMensaje)) {
+    } else if (miMensaje.length == 0 || /^\s+$/.test(miMensaje)) {
         alert('El mensaje esta vacio!');
-    }else{
-        $(document).ready(function () {
-            $("#validateButton").click(function () {
-                $("#Contacto").load("Enlace.html");
-            });
-        });
-
-        $(document).ready(function () {
-            $("#validateButton").click(function () {
-                $(this).hide();
-            });
-        });
-
+    } else {
+        document.getElementById('row').innerHTML = "Gracias por contactarnos, en pocos minutos nos comunicaremos.";
     }
 
 }
