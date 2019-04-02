32738f8d7c094925c7b31b4d8c14a8a9df219091
FirefoxNava <mnavarr09665@universidadean.edu.co>
2019-04-02 00:26:26 -0500

    Error Scroll

--- Js/index_js.js
+++ Js/index_js.js
@@ -3,23 +3,6 @@ $(document).ready(function () {
 
 })
 
-/*Scroll Suave*/
-
-
-$(document).ready(function() {
-
-  $('a[href^="#"]').click(function() {
-    var destino = $(this.hash);
-    if (destino.length == 0) {
-      destino = $('a[name="' + this.hash.substr(1) + '"]');
-    }
-    if (destino.length == 0) {
-      destino = $('html');
-    }
-    $('html, body').animate({ scrollTop: destino.offset().top }, 500);
-    return false;
-  });
-});
 
 
 
@@ -37,7 +20,6 @@ function procs(){
         return false;
     })
 }
-
 
 
 /**
