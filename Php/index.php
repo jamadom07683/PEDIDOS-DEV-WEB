7a1c8864b04faa6f9d558abd705e10744a5bd2f0
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-04-09 08:14:41 -0500

    Primer commit Php

---
+++ Php/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <h1> <?php echo 'Pagina en php'; ?></h1>
+    <h2> Hola cliente</h2>
+</body>
+</html>
