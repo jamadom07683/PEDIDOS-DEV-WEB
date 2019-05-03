761f3ef814847a29d040c719c6210dae8a9aac4c
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-05-03 08:11:56 -0500

    Inicio de Sesión

---
+++ inicioSesion.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Iniciar Sesión</title>
+
+    <h1> ¿Que es lo que jodia tanto mk?</h1>
+</head>
+<body>
+
+</body>
+</html>
