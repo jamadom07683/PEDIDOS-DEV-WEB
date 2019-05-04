575a586d6e518f842464318906f06ba2686b2575
jamadom07683 <jamadom07683@universidadean.edu.co>
2019-05-04 11:15:27 -0500

    Php

--- Php/inicioSesion.php
+++ Php/inicioSesion.php
@@ -13,8 +13,6 @@
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 
-    <body background="Images/Fondo.jpg"></body>
-
     <title>Iniciar Sesión</title>
 
 </head>
