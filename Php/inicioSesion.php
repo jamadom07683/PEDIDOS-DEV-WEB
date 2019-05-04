c5cc342a39b851cacdf5cc7ee70fa1685de8e16d
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-05-04 12:12:00 -0500

    Barra de navegación Inicio de Sesion

--- Php/inicioSesion.php
+++ Php/inicioSesion.php
@@ -19,6 +19,28 @@
 
 <body>
 
+    <!-- Barra de Navegación - Inicio -->
+
+    <header class="nav_bar fixed-top">
+
+        <div class="menu_bar">
+            <p href="#" class="boton_menu"><span class="icon-menu"></span>Menu</p>
+        </div>
+
+        <nav class="barra">
+            <ul class="items_list">
+                <li class="items_nav"><a href="#"><span class="icon-home3"></span>Inicio</a></li>
+                <li class="items_nav"><a href="#AcercaDe"><span class="icon-earth"></span>Acerca De</a></li>
+                <li class="items_nav"><a href="#Productos"><span class="icon-briefcase"></span>Productos</a></li>
+                <li class="items_nav"><a href="#about"><span class="icon-rocket"></span>Equipo de Trabajo</a></li>
+                <li class="items_nav"><a href="#Contacto"><span class="icon-mail"></span>Contactenos</a></li>
+                <li class="items_nav"><a href="#header"><span class="fas fa-shopping-cart"></span>Carrito <span id="items_count">0</span></a></li>
+                <li class="items-nav"><a href="Php/inicioSesion.php"><i class="fas fa-sign-in-alt"></i> Inicio de Sesión</a>></li>
+
+            </ul>
+        </nav>
+    </header>
+
     <h1> Iniciar Sesión </h1>
 
     <form action="ingreso.php" method="post">
