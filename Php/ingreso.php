03d60133a1cdf6659fcbeb4eac46055cd1879265
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-05-04 11:50:18 -0500

    Inicio de Sesión

--- Php/ingreso.php
+++ Php/ingreso.php
@@ -60,7 +60,6 @@
         <br>
         <a href="inicioSesion.html">Volver</a>
     </section>
-    ?>
 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -68,4 +67,3 @@
     <script src="Js/index_js.js" type="text/javascript"></script>
 
 </body>
-
