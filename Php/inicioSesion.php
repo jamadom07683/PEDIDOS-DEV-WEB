ad2f2350eeff75a73ff30005be18918a9317ce44
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-05-04 12:57:37 -0500

    Barra de navegacion Index

--- Php/inicioSesion.php
+++ Php/inicioSesion.php
@@ -20,28 +20,22 @@
 <body>
 
 
-    
-=======
-    <!-- Barra de Navegación - Inicio -->
+    <div class="menu_bar">
+        <p href="#" class="boton_menu"><span class="icon-menu"></span>Menu</p>
+    </div>
 
-    <header class="nav_bar fixed-top">
+    <nav class="barra">
+        <ul class="items_list">
+            <li class="items_nav"><a href="../index.html"><span class="icon-home3"></span>Inicio</a></li>
+            <li class="items_nav"><a href="../index.html/#AcercaDe"><span class="icon-earth"></span>Acerca De</a></li>
+            <li class="items_nav"><a href="#Productos"><span class="icon-briefcase"></span>Productos</a></li>
+            <li class="items_nav"><a href="#about"><span class="icon-rocket"></span>Equipo de Trabajo</a></li>
+            <li class="items_nav"><a href="#Contacto"><span class="icon-mail"></span>Contactenos</a></li>
+            <li class="items_nav"><a href="#header"><span class="fas fa-shopping-cart"></span>Carrito <span id="items_count">0</span></a></li>
+            <li class="items-nav"><a href="Php/inicioSesion.php"><i class="fas fa-sign-in-alt"></i> Inicio de Sesión</a>></li>
 
-        <div class="menu_bar">
-            <p href="#" class="boton_menu"><span class="icon-menu"></span>Menu</p>
-        </div>
-
-        <nav class="barra">
-            <ul class="items_list">
-                <li class="items_nav"><a href="../index.html"><span class="icon-home3"></span>Inicio</a></li>
-                <li class="items_nav"><a href="../index.html/#AcercaDe"><span class="icon-earth"></span>Acerca De</a></li>
-                <li class="items_nav"><a href="#Productos"><span class="icon-briefcase"></span>Productos</a></li>
-                <li class="items_nav"><a href="#about"><span class="icon-rocket"></span>Equipo de Trabajo</a></li>
-                <li class="items_nav"><a href="#Contacto"><span class="icon-mail"></span>Contactenos</a></li>
-                <li class="items_nav"><a href="#header"><span class="fas fa-shopping-cart"></span>Carrito <span id="items_count">0</span></a></li>
-                <li class="items-nav"><a href="Php/inicioSesion.php"><i class="fas fa-sign-in-alt"></i> Inicio de Sesión</a>></li>
-
-            </ul>
-        </nav>
+        </ul>
+    </nav>
     </header>
 
     <br>
