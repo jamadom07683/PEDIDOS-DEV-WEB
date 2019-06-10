da589fd727c1873a61da43bf58330fb89c295f49
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-06-10 09:05:30 -0500

    Cambios Insert Into

--- Php/resultRegistrar.php
+++ Php/resultRegistrar.php
@@ -12,7 +12,7 @@ $post="";
 if ($Nombre=="" || $Usuario=="" || $Contrasena=""){
         echo 'alert("Datos incorrectos o incompletos")';
 }
-$sql= "INSERT INTO Usuario (Nombre,Usuario,Contraseña) VALUES ('".$Nombre."','".$Usuario."','".Contrasena."')";
+$sql= "INSERT INTO Usuario (Nombre,Usuario,Contraseña) VALUES ('".$Nombre."','".$Usuario."','".$Contrasena."')";
 
 if (mysqli_query($conn, $sql) === TRUE) {
     echo 'alert("Registro completo")';
