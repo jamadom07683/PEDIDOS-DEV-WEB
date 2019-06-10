c0a5d6082377dd4e888389253151e76219d11aa8
jamadom07683 <jamadom07683@universidadean.edu.co>
2019-06-10 10:46:55 -0500

    intento

--- Php/resultRegistrar.php
+++ Php/resultRegistrar.php
@@ -8,14 +8,17 @@ $Usuario = $_POST['usuario'];
 $Contrasena = $_POST['contraseña'];
 
 
-$sql = "INSERT INTO Usuario (Nombre,Usuario,Contraseña) VALUES ('".$_POST['nombre']."' , '".$_POST['usuario']."' , '".$_POST['contraseña']."')";
-$result = mysqli_query($conn, $sql);
-if($result)
-{
-	header("Location: inicioSesion.php");
-}
-else
-{
-	echo "Error :".$sql;
+if (isset($_POST['Nombre']) && isset($_POST['Usuario']) && isset($_POST['Contraseña'])){
+        $sentenciaSQL = "INSERT INTO `Usuario` ( `Nombre`, `Usuario`, `Contraseña`) ";
+        $sentenciaSQL = $sentenciaSQL."VALUES ( '";
+        $sentenciaSQL = $sentenciaSQL."".$_POST['Nombre']."', '";
+        $sentenciaSQL = $sentenciaSQL."".$_POST['Usuario']."', '";
+        $sentenciaSQL = $sentenciaSQL."".$_POST['Contraseña']."', '";
+        if ($conexion->query($sentenciaSQL) === TRUE) {
+            echo "Usuario creado";
+        } else {
+            echo "Error: " . $sentenciaSQL . "<br>" . $conexion->error;
+        }
+        $conexion->close();
 }
 ?>
