9cf410009398bbd475bca4024320fa017a45c7c9
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-06-10 09:04:39 -0500

    Cambios Insert Into

--- Php/resultRegistrar.php
+++ Php/resultRegistrar.php
@@ -1,84 +1,27 @@
 <?php
 require_once('connection.php');
 
-/*$Nombre = $Usuario = $Contraseña = '';
+//$Nombre = $Usuario = $Contraseña = '';
 
 
 $Nombre = $_POST['nombre'];
 $Usuario = $_POST['usuario'];
-$Contrasena = $_POST['contraseña'];*/
+$Contrasena = $_POST['contraseña'];
+$post="";
 
+if ($Nombre=="" || $Usuario=="" || $Contrasena=""){
+        echo 'alert("Datos incorrectos o incompletos")';
+}
+$sql= "INSERT INTO Usuario (Nombre,Usuario,Contraseña) VALUES ('".$Nombre."','".$Usuario."','".Contrasena."')";
 
-$message = '';
-
-
-if (!empty($_POST['nombre']) && !empty($_POST['usuario']) && !empty($_POST['contraseña'])){
-
-    //$sql = "INSERT INTO Usuario ( Nombre,Usuario,Contraseña ) VALUES (
-    //'".$_POST['nombre']."',
-    //'".$_POST['usuario']."',
-    //'".$_POST['contraseña']."'
-    //)";
-
-    $sql = "INSERT INTO Usuario (Nombre,Usuario,Contraseña) VALUES (
-    '".$_POST['nombre']."',
-    '".$_POST['usuario']."',
-    '".$_POST['contraseña']."'
-    )";
-
-    /*
-    if ($conn -> query($sql) == true){
-
-        echo '<br>See Insertó el usuario';
-
-    }else{
-
-        echo '<br>paila ese man no se deja';
-    }
-    */
-
-    if (mysqli_query($conn, $sql)) {
-        echo "New record created successfully";
-    } else {
-        echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
-    }
-
-
-    mysqli_close($conn);
-
+if (mysqli_query($conn, $sql) === TRUE) {
+    echo 'alert("Registro completo")';
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
+mysqli_close( $conn );
 
 
-
-/*
-$message = '';
-
-if(!empty($_POST['nombre']) && !empty($_POST['usuario']) && !empty($_POST['contraseña'])){
-    $sql ="INSERT INTO Usuario (Nombre,Usuario,Contraseña) VALUES (:nombre,:usuario,:contraseña)";
-    $stmt = $conn->prepare($sql);
-
-    $stmt -> bindParam(':nombre', $_POST['nombre']);
-
-    $stmt ->bindParam(':usuario',$_POST['usuario']);
-
-    $password = password_hash($_POST['contraseña'], PASSWORD_BCRYPT);
-    $stmt->bindParam(':contraseña', $password);
-
-     if ($stmt->execute()) {
-      $message = 'Successfully created new user';
-    } else {
-      $message = 'Sorry there must have been an issue creating your account';
-    }
-}
-
-
-
-
-
-
-
-
-*/
 
 ?>
