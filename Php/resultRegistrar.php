ee8c6802bc124c703752aed57466760666eb7272
jamadom07683 <jamadom07683@universidadean.edu.co>
2019-06-10 11:42:27 -0500

    echo

--- Php/resultRegistrar.php
+++ Php/resultRegistrar.php
@@ -1,21 +1,19 @@
 <?php
 require_once('connection.php');
-$Nombre = $Usuario = $Contraseña = '';
+/*$Nombre = $Usuario = $Contraseña = '';
 
 
 $Nombre = $_POST['nombre'];
 $Usuario = $_POST['usuario'];
-$Contrasena = $_POST['contraseña'];
+$Contrasena = $_POST['contraseña'];*/
 
 
 if (isset($_POST['nombre']) && isset($_POST['usuario']) && isset($_POST['contraseña'])){
 $query = "INSERT INTO Usuario ( Nombre,Usuario,Contraseña) VALUES ('".$_POST['nombre']."','".$_POST['usuario']."', '".$_POST['contraseña']."')";
 echo $query;
 if ($conn -> query($query)){
-echo '<br>Se Insertó el usuario';
+echo '<br>See Insertó el usuario';
 }else{
 echo '<br>paila ese man no se deja';
 }
 ?>
-
-
