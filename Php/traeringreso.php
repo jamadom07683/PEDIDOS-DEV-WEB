6f1f6a7723bcbc22bd44266b75908767f3f684ee
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-06-10 10:20:25 -0500

    Cambios LogIn

--- Php/traeringreso.php
+++ Php/traeringreso.php
@@ -12,22 +12,17 @@ $result = mysqli_query($conn, $sql);
 $datos = mysqli_fetch_array($result);
 
 if(mysqli_num_rows($result) == 0){
-	/*while($row = mysqli_fetch_assoc($result)){
+	while($row = mysqli_fetch_assoc($result)){
 		$id = $row["ID"];
 		$Usuario = $row["user"];
 		session_start();
 		$_SESSION['id'] = $id;
 		$_SESSION['password'] = $Contrasena;
 	}
-	echo "Bienvenido";
-}
-else
-{
-	echo "Invalid usser or password";*/
     echo "Invalid usser or password";
-
 }
 else{
-    echo "Bienvenido";
+	echo "Bienvenido";
 }
+
 ?>
