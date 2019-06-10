6c94498c26101846c8e79c109e31601451f530a9
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-06-10 07:34:00 -0500

    Cambios Insert Into

--- Php/resultRegistrar.php
+++ Php/resultRegistrar.php
@@ -9,13 +9,23 @@ $Usuario = $_POST['usuario'];
 $Contrasena = $_POST['contraseña'];*/
 
 
+/*
 if (isset($_POST['nombre']) && isset($_POST['usuario']) && isset($_POST['contraseña'])){
-$sql = "INSERT INTO Usuario ( Nombre,Usuario,Contraseña ) VALUES ('".$_POST['nombre']."','".$_POST['usuario']."', '".$_POST['contraseña']."')";
-echo $query;
+
+    $sql = "INSERT INTO Usuario ( Nombre,Usuario,Contraseña ) VALUES (
+    '".$_POST['nombre']."',
+    '".$_POST['usuario']."',
+    '".$_POST['contraseña']."'
+    )";
+    echo $query;
     if ($conn -> query($sql) == true){
     echo '<br>See Insertó el usuario';
     }else{
     echo '<br>paila ese man no se deja';
     }
 }
+*/
+
+echo $_POST['nombre'];
+echo $_POST['usuario'];
 ?>
