90ee707c749130618f509d6b317d5a4e8694524a
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-06-10 08:35:05 -0500

    Cambios Insert Into

--- Php/resultRegistrar.php
+++ Php/resultRegistrar.php
@@ -33,11 +33,14 @@ if (!empty($_POST['nombre']) && !empty($_POST['usuario']) && !empty($_POST['cont
     }
     */
 
-    if ($stmt->execute()) {
-        echo '<br>Successfully created new user';
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
     } else {
-        echo '<br>Sorry there must have been an issue creating your account';
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
+
+
+    mysqli_close($conn);
 
 
 }
