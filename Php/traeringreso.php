e7d3c1474bce14612a44b77cd844449f9f3f53a1
jamadom07683 <jamadom07683@universidadean.edu.co>
2019-06-10 09:06:19 -0500

    mysqli

--- Php/traeringreso.php
+++ Php/traeringreso.php
@@ -6,17 +6,20 @@
     $Usuario = stripcslashes($Usuario);
     $Contraseña = stripcslashes($Contraseña);
 
-    $Usuario=mysql_real_escape_string($Uusario);
-    $Contraseña = mysql_real_escape_string($Contraseña);
+    $Usuario=mysqli_real_escape_string($Uusario);
+    $Contraseña = mysqli_real_escape_string($Contraseña);
 
     //conexión
-    mysql_connect("localhost","server","server");
-    mysql_select_db("Proyecto");
+    $host = 'localhost';
+    $user = 'server';
+    $password = 'server';
+    $db = 'Proyecto';
+    $conexion = new mysqli($host, $user, $password, $db);
 
     //resultado
-    $result= mysql_query("select*from Usuario where Usuario='$Usuario' and Contraseña = '$Contraseña'")
-        or die("Failed to query database".mysql_error());
-    $row=mysql_fetch_array($result);
+    $result= $mysqli->query("select*from Usuario where Usuario='$Usuario' and Contraseña = '$Contraseña'")
+        or die("Failed to query database".mysqli_error());
+    $row=mysqli_fetch_array($result);
     if($row['Usuario']==$Usuario && $row['Contraseña']==$Contraseña){
         echo "Login successful!!! Welcome".$row['Usuario'];
     }else{
