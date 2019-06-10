08490ce1d5a77198f11e37e9b926b1a151b7df7e
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-06-10 09:27:44 -0500

    encriptamiento de password

--- Php/resultRegistrar.php
+++ Php/resultRegistrar.php
@@ -8,9 +8,10 @@ $Nombre = $_POST['nombre'];
 $Usuario = $_POST['usuario'];
 $Contrasena = $_POST['contraseña'];
 
+$Password = password_hash($_POST['contraseña'], PASSWORD_BCRYPT);
 
 if (!empty( $_POST['nombre']) && !empty($_POST['usuario']) && !empty($_POST['contraseña'])){
-    $sql= "INSERT INTO Usuario (Nombre,Usuario,Contrasena) VALUES ('".$Nombre."','".$Usuario."','".$Contrasena."')";
+    $sql= "INSERT INTO Usuario (Nombre,Usuario,Contrasena) VALUES ('".$Nombre."','".$Usuario."','".$Password."')";
 
     if (mysqli_query($conn, $sql) === TRUE) {
         echo '<script language="javascript">alert("Registro completo");</script>';
