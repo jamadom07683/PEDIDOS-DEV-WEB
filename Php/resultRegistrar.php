31a595256a1940ece8dd335b05a1b28bb54ac39b
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-06-10 09:22:46 -0500

    Insertar Usuario

--- Php/resultRegistrar.php
+++ Php/resultRegistrar.php
@@ -9,17 +9,20 @@ $Usuario = $_POST['usuario'];
 $Contrasena = $_POST['contraseña'];
 
 
-if (!empty( $_POST['nombre']) || !empty($_POST['usuario']) || !empty($_POST['contraseña'])){
-        echo '<script language="javascript">alert("Datos incompletos");</script>';
+if (!empty( $_POST['nombre']) && !empty($_POST['usuario']) && !empty($_POST['contraseña'])){
+    $sql= "INSERT INTO Usuario (Nombre,Usuario,Contrasena) VALUES ('".$Nombre."','".$Usuario."','".$Contrasena."')";
+
+    if (mysqli_query($conn, $sql) === TRUE) {
+        echo '<script language="javascript">alert("Registro completo");</script>';
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+}
+else{
+    echo '<script language="javascript">alert("Datos incompletos");</script>';
 }
 
-$sql= "INSERT INTO Usuario (Nombre,Usuario,Contrasena) VALUES ('".$Nombre."','".$Usuario."','".$Contrasena."')";
-
-if (mysqli_query($conn, $sql) === TRUE) {
-    echo '<script language="javascript">alert("Registro completo");</script>';
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
 
 mysqli_close( $conn );
 
