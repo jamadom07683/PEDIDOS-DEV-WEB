76c64a8b075ce1d9ccfeeba877d191dbddd2d48e
OmarFabianDiaz <odiazac26363@universidadean.edu.co>
2019-06-10 23:59:25 -0500

    Detalles un solo producto

--- Php/detallesCarro.php
+++ Php/detallesCarro.php
@@ -65,12 +65,14 @@
 
                 while ($pro=mysqli_fetch_array($result)){
                 ?>
+                    <div class="col-lg-3 col-md-3 col-sm-12 item">
                     <center>
                         <a class="img_pro des_pal" href="#"><img src="../Images/<?php echo $pro['Imagen'];?>" class="img-fluid"></a>
                         <span class="text-white"><?php echo $pro['Nombre']?></span><br>
                         <span class="text-white">Precio: <?php echo $pro['Precio']?></span><br>
                         <a href="detallesCarro.php?id=<?php echo $pro['Id_producto'];?>"> Detalles</a>
                     </center>
+                    </div>
 
             <?php
                 }
